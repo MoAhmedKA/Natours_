99bf759c55c580fa2a458686123dc0f4fda6e39f
MoAhmedKA <momenahmed20021212@gmail.com>
2023-12-12 08:05:44 -0800

    :reverting all of helmet adjustments

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -27,7 +27,7 @@ exports.login = (req, res) => {
   console.log(req.headers)
   res
     .status(200)
-    // .set('Content-Security-Policy', "connect-src 'self' http://127.0.0.1:3000/")
+     .set('Content-Security-Policy', "connect-src 'self' http://127.0.0.1:3000/")
     .render('login', {
       title: 'Log into your account'
     });

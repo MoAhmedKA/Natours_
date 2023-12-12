ecece35f5c6bc65d67693654d51ea0b5f2460be8
MoAhmedKA <momenahmed20021212@gmail.com>
2023-12-12 07:58:19 -0800

    CSP shit again i hate my life

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Set Sucurity HTTP Header //
   app.use(
     helmet({
-      contentSecurityPolicy: true,
+      contentSecurityPolicy: false,
     
     })
   );
